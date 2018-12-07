d13d558d52426da45315c4f016cecadfb4567c36
Ptaq <antyplastik@gmail.com>
2018-12-07 16:00:05 +0100

    BMIanalyzerTest pass

--- src/test/java/BMIanalyzerTest.java
+++ src/test/java/BMIanalyzerTest.java
@@ -58,29 +58,29 @@ public class BMIanalyzerTest {
                 new Object[]{Gender.FEMALE, 19, true},
                 new Object[]{Gender.FEMALE, 210, false},
                 new Object[]{Gender.FEMALE, 23, true},
-                new Object[]{Gender.FEMALE, 24, false},
+                new Object[]{Gender.FEMALE, 23.01, false},
         };
     }
 
     @Test
     @Parameters(method = "testMethodToCheckIfPersonIsOverweight")
     public void checkIfPersonIsOverweight(Gender gender, double bmi, boolean expected) { // < =========================== Napisac
-
+        assertThat(bmiAnalyzer.checkIfOverweight(gender,bmi),is(expected));
     }
 
     private Object[] testMethodToCheckIfPersonIsOverweight() {
         return new Object[]{
-                new Object[]{Gender.MALE, 20.99, false},
-                new Object[]{Gender.MALE, 21, true},
-                new Object[]{Gender.MALE, 23, false},
-                new Object[]{Gender.MALE, 25, true},
-                new Object[]{Gender.MALE, 25.01, false},
+                new Object[]{Gender.MALE, 25, false},
+                new Object[]{Gender.MALE, 25.01, true},
+                new Object[]{Gender.MALE, 230, true},
+                new Object[]{Gender.MALE, 31, true},
+                new Object[]{Gender.MALE, 31.01, true},
 
-                new Object[]{Gender.FEMALE, 18.99, false},
-                new Object[]{Gender.FEMALE, 19, true},
-                new Object[]{Gender.FEMALE, 21, false},
-                new Object[]{Gender.FEMALE, 23, true},
-                new Object[]{Gender.FEMALE, 24, false},
+                new Object[]{Gender.FEMALE, 23, false},
+                new Object[]{Gender.FEMALE, 23.01, true},
+                new Object[]{Gender.FEMALE, 210, true},
+                new Object[]{Gender.FEMALE, 29, true},
+                new Object[]{Gender.FEMALE, 24, true},
         };
     }
 }
